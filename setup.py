bfa2e9e46aae5d173dc729e73a8251e212e11dff
Richard Neumann <mail@richard-neumann.de>
2021-04-15 12:22:52 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+"""Installations script."""
+
+from setuptools import setup
+
+
+setup(
+    name='html5ds',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    python_requires='>=3.8',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info@homeinfo.de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r.neumann@homeinfo.de>',
+    py_modules=['html5ds'],
+    entry_points={'console_scripts': ['html5ds = html5ds:main']},
+    description=('Browser wrapper for HTML5 digital sigange applications.')
+)
