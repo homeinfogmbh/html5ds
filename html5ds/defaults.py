bfcb09f088d6c23e16e114f897ea0d1530bae213
Richard Neumann <mail@richard-neumann.de>
2021-10-19 18:16:51 +0200

    Use plain class for Defaults

--- html5ds/defaults.py
+++ html5ds/defaults.py
@@ -1,20 +1,20 @@
 """Default values."""
 
-from enum import Enum
-
 from html5ds.types import Position, Resolution
 
 
 __all__ = ['Defaults']
 
 
-class Defaults(Enum):
+class Defaults:     # pylint: disable=R0903
     """Default configuration values."""
 
-    EXECUTABLE = '/usr/bin/chromium'
-    POSITION = Position(0, 0)
-    RESOLUTION = Resolution(1920, 1080)
-    DISK_CACHE_DIR = '/dev/null'
-    VERBOSITY = 1
-    URL = 'http://localhost/index.html'
-    DISPLAY = ':0'
+    __slots__ = ()
+
+    EXECUTABLE: str = '/usr/bin/chromium'
+    POSITION: Position = Position(0, 0)
+    RESOLUTION: Resolution = Resolution(1920, 1080)
+    DISK_CACHE_DIR: str = '/dev/null'
+    VERBOSITY: int = 1
+    URL: str = 'http://localhost/index.html'
+    DISPLAY: str = ':0'
--- html5ds/main.py
+++ html5ds/main.py
@@ -17,8 +17,7 @@ __all__ = ['get_command', 'get_environ', 'main']
 def get_command(config: ConfigParser = CONFIG) -> Iterator[str]:
     """Returns the command to run."""
 
-    yield config.get(
-        'webbrowser', 'executable', fallback=Defaults.EXECUTABLE.value)
+    yield config.get('webbrowser', 'executable', fallback=Defaults.EXECUTABLE)
 
     if config.getboolean('webbrowser', 'kiosk', fallback=True):
         yield '--kiosk'
@@ -26,40 +25,39 @@ def get_command(config: ConfigParser = CONFIG) -> Iterator[str]:
     if config.getboolean('webbrowser', 'fullscreen', fallback=True):
         yield '--fullscreen'
 
-    width = config.getint('screen', 'width',fallback=Defaults.RESOLUTION.value.width)
+    width = config.getint('screen', 'width',fallback=Defaults.RESOLUTION.width)
     height = config.getint(
-        'screen', 'height', fallback=Defaults.RESOLUTION.value.height)
+        'screen', 'height', fallback=Defaults.RESOLUTION.height)
     window_size = ','.join(map(str, (width, height)))
     yield f'--window-size={window_size}'
 
-    posx = config.getint('screen', 'posx', fallback=Defaults.POSITION.value.x)
-    posy = config.getint('screen', 'posy', fallback=Defaults.POSITION.value.y)
+    posx = config.getint('screen', 'posx', fallback=Defaults.POSITION.x)
+    posy = config.getint('screen', 'posy', fallback=Defaults.POSITION.y)
     window_position = ','.join(map(str, (posx, posy)))
     yield f'--window-position={window_position}'
 
     disk_cache_dir = config.get(
-        'webbrowser', 'disk_cache_dir', fallback=Defaults.DISK_CACHE_DIR.value)
+        'webbrowser', 'disk_cache_dir', fallback=Defaults.DISK_CACHE_DIR)
     yield f'--disk-cache-dir={disk_cache_dir}'
 
     if config.getboolean('webbrowser', 'logging', fallback=True):
         yield '--enable-logging'
 
     verbosity = config.getint(
-        'webbrowser', 'verbosity', fallback=Defaults.VERBOSITY.value)
+        'webbrowser', 'verbosity', fallback=Defaults.VERBOSITY)
     yield f'--v={verbosity}'
 
     if args := config.get('webbrowser', 'options', fallback=None):
         yield from args.split()
 
-    yield config.get('application', 'url', fallback=Defaults.URL.value)
+    yield config.get('application', 'url', fallback=Defaults.URL)
 
 
 def get_environ(config: ConfigParser = CONFIG) -> dict:
     """Returns the environment for the subprocess."""
 
     env = environ.copy()
-    env['DISPLAY'] = config.get(
-        'screen', 'display', fallback=Defaults.DISPLAY.value)
+    env['DISPLAY'] = config.get('screen', 'display', fallback=Defaults.DISPLAY)
     return env
 
 
