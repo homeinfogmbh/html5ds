5813737f55a7e8094e4a51108c69ce91a6214905
Richard Neumann <mail@richard-neumann.de>
2021-10-25 16:52:23 +0200

    Make defaults plain variables

--- html5ds/command.py
+++ html5ds/command.py
@@ -5,7 +5,13 @@ from os import environ
 from typing import Iterator
 
 from html5ds.config import CONFIG
-from html5ds.defaults import Defaults
+from html5ds.defaults import DISK_CACHE_DIR
+from html5ds.defaults import DISPLAY
+from html5ds.defaults import EXECUTABLE
+from html5ds.defaults import POSITION
+from html5ds.defaults import RESOLUTION
+from html5ds.defaults import URL
+from html5ds.defaults import VERBOSITY
 
 
 __all__ = ['get_command', 'get_environ']
@@ -14,7 +20,7 @@ __all__ = ['get_command', 'get_environ']
 def get_command(config: ConfigParser = CONFIG) -> Iterator[str]:
     """Returns the command to run."""
 
-    yield config.get('webbrowser', 'executable', fallback=Defaults.EXECUTABLE)
+    yield config.get('webbrowser', 'executable', fallback=EXECUTABLE)
 
     if config.getboolean('webbrowser', 'kiosk', fallback=True):
         yield '--kiosk'
@@ -22,36 +28,32 @@ def get_command(config: ConfigParser = CONFIG) -> Iterator[str]:
     if config.getboolean('webbrowser', 'fullscreen', fallback=True):
         yield '--fullscreen'
 
-    width = config.getint(
-        'screen', 'width', fallback=Defaults.RESOLUTION.width)
-    height = config.getint(
-        'screen', 'height', fallback=Defaults.RESOLUTION.height)
+    width = config.getint('screen', 'width', fallback=RESOLUTION.width)
+    height = config.getint('screen', 'height', fallback=RESOLUTION.height)
     yield f'--window-size={width},{height}'
 
-    posx = config.getint('screen', 'posx', fallback=Defaults.POSITION.x)
-    posy = config.getint('screen', 'posy', fallback=Defaults.POSITION.y)
+    posx = config.getint('screen', 'posx', fallback=POSITION.x)
+    posy = config.getint('screen', 'posy', fallback=POSITION.y)
     yield f'--window-position={posx},{posy}'
 
-    disk_cache_dir = config.get(
-        'webbrowser', 'disk_cache_dir', fallback=Defaults.DISK_CACHE_DIR)
-    yield f'--disk-cache-dir={disk_cache_dir}'
+    cache = config.get('webbrowser', 'disk_cache_dir', fallback=DISK_CACHE_DIR)
+    yield f'--disk-cache-dir={cache}'
 
     if config.getboolean('webbrowser', 'logging', fallback=True):
         yield '--enable-logging'
 
-    verbosity = config.getint(
-        'webbrowser', 'verbosity', fallback=Defaults.VERBOSITY)
+    verbosity = config.getint('webbrowser', 'verbosity', fallback=VERBOSITY)
     yield f'--v={verbosity}'
 
     if args := config.get('webbrowser', 'options', fallback=None):
         yield from args.split()
 
-    yield config.get('application', 'url', fallback=Defaults.URL)
+    yield config.get('application', 'url', fallback=URL)
 
 
 def get_environ(config: ConfigParser = CONFIG) -> dict:
     """Returns the environment for the subprocess."""
 
     env = environ.copy()
-    env['DISPLAY'] = config.get('screen', 'display', fallback=Defaults.DISPLAY)
+    env['DISPLAY'] = config.get('screen', 'display', fallback=DISPLAY)
     return env
--- html5ds/defaults.py
+++ html5ds/defaults.py
@@ -3,18 +3,21 @@
 from html5ds.types import Position, Resolution
 
 
-__all__ = ['Defaults']
+__all__ = [
+    'DISK_CACHE_DIR',
+    'DISPLAY',
+    'EXECUTABLE',
+    'POSITION',
+    'RESOLUTION',
+    'URL',
+    'VERBOSITY'
+]
 
 
-class Defaults:     # pylint: disable=R0903
-    """Default configuration values."""
-
-    __slots__ = ()
-
-    EXECUTABLE: str = '/usr/bin/chromium'
-    POSITION: Position = Position(0, 0)
-    RESOLUTION: Resolution = Resolution(1920, 1080)
-    DISK_CACHE_DIR: str = '/dev/null'
-    VERBOSITY: int = 1
-    URL: str = 'http://localhost/index.html'
-    DISPLAY: str = ':0'
+DISK_CACHE_DIR: str = '/dev/null'
+DISPLAY: str = ':0'
+EXECUTABLE: str = '/usr/bin/chromium'
+POSITION: Position = Position(0, 0)
+RESOLUTION: Resolution = Resolution(1920, 1080)
+URL: str = 'http://localhost/index.html'
+VERBOSITY: int = 1
